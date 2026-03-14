7dcb132a7b6116d792361fd733b66c4c94263f90
Alfred Varghese Jose <alfredvjose17@gmail.com>
2026-03-14 13:52:14 -0400

    initial commit

---
+++ restaurant/views.py
@@ -0,0 +1,33 @@
+# from django.http import HttpResponse
+from django.shortcuts import render
+from .forms import BookingForm
+from .models import Menu
+
+
+
+# Create your views here.
+def home(request):
+    return render(request, 'index.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def book(request):
+    form = BookingForm()
+    if request.method == 'POST':
+        form = BookingForm(request.POST)
+        if form.is_valid():
+            form.save()
+    context = {'form':form}
+    return render(request, 'book.html', context)
+
+# Add your code here to create new views
+def menu(request):
+    menu = Menu.objects.all()
+    context = {'menu': menu}
+    return render(request, 'menu.html', context)
+
+def display_menu_item(request, pk=None):
+    menu_item = Menu.objects.get(id=pk)
+    context = {'menu_item': menu_item}
+    return render(request, 'menu_item.html', context)
